b9a98ed141cd1cb7c50c0c62d97914ff4b26307b
sukonvu <yumachan.0905.sukonvuhime@gmail.com>
2023-06-11 20:19:43 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_week
@@ -33,10 +33,15 @@ class CalendarsController < ApplicationController
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
-      end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+      end 
+
+      # wday_num = # wdayメソッドを用いて取得した数値
+      # if #「wday_numが7以上の場合」という条件式
+      #   wday_num = wday_num -7
+      # end
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans} #, :wday => wdaysから値を取り出す記述}
       @week_days.push(days)
     end
-
   end
 end
